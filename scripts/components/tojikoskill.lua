0b78da9cf7e919bb746f541ed260bc8ba9e995a5
Yukari7777 <yys14239102@gmail.com>
2022-09-13 21:38:04 +0900

    번개 기능구현

--- scripts/components/tojikoskill.lua
+++ scripts/components/tojikoskill.lua
@@ -7,6 +7,8 @@ local TojikoSkill = Class(function(self, inst)
     self.thundernumcast = 0
 end)
 
+-- 번개와 벨런스 조정이 필요하다고 생각되면 정의된 함수 바로 위의 local 값들(ex. THUNDER_COOLDOWN 등)은 수정하시면 됩니다.
+
 function TojikoSkill:CanThunderStrike()
     return self.thundertask == nil or self.thundercooldowntask == nil
 end
@@ -31,6 +33,8 @@ end
 
 local THUNDER_SEQUENCE = 3 -- 첫 시전 후 연속으로 번개를 칠 수 있는 시간
 local THUNDER_MAX_USE = 3 -- 번개 연속 시전 횟수
+local THUNDER_RADIUS = 3 -- 낙뢰 범위
+local THUNDER_LIGHTNINGROD_SEARCH_RANGE = 10 -- 피뢰침 인식 범위
 function TojikoSkill:ThunderStrike()
     -- self.thundernumcast = self.thundernumcast + 1
     -- if self.thundertask == nil then -- 최초 시전시
@@ -40,7 +44,48 @@ function TojikoSkill:ThunderStrike()
     -- elseif self.thundernumcast == THUNDER_MAX_USE - 1 then -- 연속 3회 시전시 즉시 쿨타임 적용
     --     self:CooldownThunder()
     -- end
-    SpawnPrefab("lightning").Transform:SetPosition(self.thunderpos:Get()) -- ISSUE : 번개FX가 마우스 커서 지점에 정확히 맞지 않음, 필요시 수정
+    self.inst:AddTag("ignorethunder") -- 시전자 자신은 번개 면역
+
+    -- 참고: 전부 자란 Knobbly Tree는 번개 치는 것을 막아주는 기능을 하지만 이 함수에선 그것을 무시합니다.
+    
+    local x, y, z = self.thunderpos:Get()
+    local rods = TheSim:FindEntities(x, y, z, THUNDER_LIGHTNINGROD_SEARCH_RANGE, {"lightningrod"})
+    local rangesq = THUNDER_LIGHTNINGROD_SEARCH_RANGE * THUNDER_LIGHTNINGROD_SEARCH_RANGE
+    local closest_rod = nil
+    for _, v in pairs(rods) do
+        local distsq = v:GetDistanceSqToPoint(x, y, z)
+        if distsq < rangesq then
+            rangesq = distsq
+            closest_rod = v
+        end
+    end
+
+    if closest_rod ~= nil then
+        SpawnPrefab("thunder").Transform:SetPosition(x, y, z)
+        closest_rod:PushEvent("lightningstrike")
+    else -- weather.lua OnSendLightningStrike()
+        SpawnPrefab("lightning").Transform:SetPosition(x-3, y, z-3) -- 좌표 조금씩 움직여서 커서지점에 맞출 필요가 있어보임
+        local targets = TheSim:FindEntities(x, y, z, THUNDER_RADIUS, nil, {"INLIMBO", "ignorethunder"})
+        for _, v in pairs(targets) do
+            if v.components.playerlightningtarget ~= nil and (v.components.health ~= nil and not v.components.health:IsInvincible()) then 
+                v.components.playerlightningtarget:DoStrike()
+            elseif v.components.health ~= nil and not v.components.health:IsInvincible() then -- playerlightningtarget.lua
+                local mult = v.components.moisture ~= nil and TUNING.ELECTRIC_WET_DAMAGE_MULT * v.components.moisture:GetMoisturePercent() or 1 -- 젖을수록 데미지 증가
+                local damage = TUNING.LIGHTNING_DAMAGE + mult * TUNING.LIGHTNING_DAMAGE
+
+                v.components.health:DoDelta(-damage, false, "lightning")
+            elseif v.components.burnable ~= nil then
+                v.components.burnable:Ignite()
+            end
+        end
+    end
+    
+    -- 번개 칠때마다 정신력 닳게 하고 싶을 경우
+    if self.inst.components.sanity ~= nil then
+        self.inst.components.sanity:DoDelta(-1)
+    end
+
+    self.inst:RemoveTag("ignorethunder")
 end
 
 return TojikoSkill
